fda5ca133392044c74865f8e12eb5a52ec01dd71
dinesh upadhya <capt.dineshupadhya@gmail.com>
2017-11-13 20:06:11 -0600

    my first repo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Display me in the CONSOLE Window.");
+
+            Console.ReadLine();
+        }
+    }
+}
