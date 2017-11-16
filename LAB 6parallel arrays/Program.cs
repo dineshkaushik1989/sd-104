06d4d31ea78a462bd555540a5dbbd6f77d3e3686
dineshkaushik1989 <33643930+dineshkaushik1989@users.noreply.github.com>
2017-11-15 20:04:27 -0600

    Add files via upload

---
+++ LAB 6parallel arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_6parallel_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Names = { "Rick Sanchez", "Morty Smith", "Jerry Smith", "Beth Smith", "Summer Smith" };
+            string[] Phone = { "555-1334", "555-3882", "555-8211", "555-1617", "555-2803" };
+            string strSearch;
+            bool isFound = false;
+            //prompt user
+            Console.WriteLine("Enter a name to search for in the phone book:");
+            strSearch = Console.ReadLine();
+
+
+            //loop through array looking for the name 
+            for (int i = 0; i < Names.Length; i++)
+            {
+
+                if (Names[i].IndexOf(strSearch) > -1)
+                {
+                    Console.WriteLine(Names[i] + " ->" + Phone[i]);
+                }
+                isFound = true;
+            }
+            if (!isFound)
+            {
+                Console.WriteLine("Name not Found");
+
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
