15cd453e758e97e22bcff9e7b8e60aba9d0f77c1
dineshkaushik1989 <33643930+dineshkaushik1989@users.noreply.github.com>
2017-11-17 20:41:43 -0600

    Add files via upload

---
+++ HW 1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_1
+{
+    class Program
+    {
+        public static string strNmae { get; private set; }
+        
+        private static int num1;
+        private static int num2;
+      
+      
+
+       
+
+        static void Main(string[] args)
+        {
+
+
+         
+
+
+            //parmot users Name
+           
+
+            //prompt user for hitnumber
+
+            Console.WriteLine("Enter a number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            //promt number for bat
+            Console.WriteLine("Enter a number");
+            num2 = Convert.ToInt32(Console.ReadLine()); 
+
+         //open results
+            Console.WriteLine(value:num1 + "+ "+ num2 +" = " + (num1 + num2));
+            Console.ReadLine();
+
+
+          //subtract result
+            Console.WriteLine(value:num1 + " - "  + num2 + " = " + (num1 - num2));
+            Console.ReadLine();
+
+          //multiplying result
+            Console.WriteLine(value:num1 + " * " + num2 + " = " + (num1 * num2));
+            Console.ReadLine();
+
+            // divide result
+            Console.WriteLine(value: num1 + "/ " + num2 + " = " + (num1 / num2));
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+   
+}
