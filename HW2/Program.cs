2ca46b1b3d4e56fee64c9805cc46e74bfa95f274
dineshkaushik1989 <33643930+dineshkaushik1989@users.noreply.github.com>
2017-11-22 18:52:08 -0600

    Add files via upload

---
+++ HW2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int F0 = 0;
+            int Secondprev = 0;
+            int Prev = 1;
+            int current, fibnum = 1;
+
+            Console.WriteLine("which fib number you wants to see");
+            fibnum =Convert.ToInt32( Console.ReadLine());
+            
+            for (int i = 0; i < fibnum; i++)
+            {
+                current = Prev + Secondprev;
+                Secondprev = Prev;
+                Prev = current;
+
+            }
+            Console.WriteLine(Prev);
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
